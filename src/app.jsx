1c9bc114e14705faea4fed73e7f38dfd4a15b331
Sardo <sardo@pixelactionstudio.com>
2017-03-28 14:00:15 +0800

    Add addToDo part

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import TodoListView from './components/TodoListView';
+import AddTodo from './components/AddTodo';
 
 const App = () => (
   <div>
     <TodoListView />
+    <AddTodo />
   </div>
 )
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers/';
-import { addTodo } from './actions';
 import App from './app';
 
 let store = createStore(todoApp);
-store.dispatch(addTodo('Hello React!'))
-store.dispatch(addTodo('Hello Redux!'))
 
 var element = document.getElementById('todo');
 
